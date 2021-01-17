8df031fe2d6d58c40ba50b3f8cda504b3d1135e4
Bryan H <34315804+bryann03@users.noreply.github.com>
2021-01-17 15:25:59 +0100

    Config Middlewares & Routes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
 //INITIALIZATIONS
 const app = express();
 
 //SETTINGS
 
-//Si existe un puertom, lo toma sino pilla el puerto 3000
+// -> Si existe un puertom, lo toma sino pilla el puerto 3000
 app.set('port', process.env.PORT | 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app-get('views'), 'partials'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
-//Seteamos toda la configuración para el 'motor de las vistas' ('view engine') llamado '.hbs'
+// -> Seteamos toda la configuración para el 'motor de las vistas' ('view engine') llamado '.hbs'
 app.set('view engine', '.hbs');
 
 //MIDDLEWARES
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'secretapp',
+    resave: true,
+    saveUninitialized: true
+}));
 
 //GLOBAL VARIABLES
 
 //ROUTES
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
 
 //STATIC FILES
 
